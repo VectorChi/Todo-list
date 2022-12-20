2b987031e4834f3ca688712a0f6a26ef29ca0100
Adimchi <adimchisylvester2@gmail.com>
2022-12-20 15:51:30 +0100

    Added strikeout and delete function

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
 let form = document.getElementById("myForm");
-const del = document.querySelector(".delete");
-const done = document.querySelector(".done");
+
 const taskList = document.querySelector(".task_list");
 const listItem = document.querySelector(".listItem");
-const options = document.querySelector(".option");
 
+// buttons
+const check = document.createElement("span");
+check.classList.add("done");
+check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check done"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
+
+const trash = document.createElement("span");
+trash.classList.add("delete");
+trash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 delete"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+
+// buttons div
+const options = document.createElement("div");
+options.classList.add("option");
+options.innerHTML = `${check.innerHTML} ${trash.innerHTML}`;
+
+console.log(options);
+// add task
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // task div
   const task = document.createElement("div");
   task.classList.add("listItem");
   task.innerHTML = `
-  <p class="task_active">${form.new_task.value}</p> ${options.outerHTML} `;
+  <p class="task_active">${form.new_task.value}</p> ${options.outerHTML}`;
 
+  // add to list and clear input
   taskList.prepend(task);
   form.new_task.value = "";
+
+  // button actions
+  const doneBtns = Array.from(document.getElementsByClassName("done"));
+  const dltBtns = Array.from(document.getElementsByClassName("delete"));
+
+  doneBtns.forEach((doneBtn) => {
+    doneBtn.addEventListener("click", (event) => {
+      parent = event.target.parentElement.parentElement;
+      task_element = parent.firstChild.nextElementSibling;
+      event.target.classList.add("hidden");
+
+      task_element.classList.add("task_finished");
+    });
+  });
+
+  dltBtns.forEach((dltBtns) => {
+    dltBtns.addEventListener("click", (event) => {
+      parent = event.target.parentElement.parentElement;
+      // task_element = parent.firstChild.nextElementSibling;
+      parent.remove();
+    });
+  });
 });
